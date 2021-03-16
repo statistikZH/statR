a3949f510db39278b8553a70a9b63f728493346a
Thomas Lo Russo <thomas.lorusso@statistik.ji.zh.ch>
2021-03-16 10:29:35 +0100

    fix palette preview function - fixes #21

--- R/display.statcol.all.R
+++ R/display.statcol.all.R
@@ -15,18 +15,18 @@ display.statcol.all<-function() {
 
   df <- data.frame(unlist(x$stattheme_data))
 
-  df$pal <- row.names(df)
+  df$pal_col <- row.names(df)
 
-  colnames(df) <- c("col","palette")
+  colnames(df) <- c("col","pal_col")
 
-  df$palette<-gsub('(.mid|.low|.high|.high|\\d$)','', df$palette)
+  df$palette<-gsub('(.mid|.low|.high|.high|\\d$)','', df$pal_col)
+
   df$palette<-gsub('\\d$','', df$palette)
 
-  gg1<-ggplot2::ggplot(df, ggplot2::aes(x=seq_along(col), y=0, color=col))+
+  gg1 <- ggplot2::ggplot(df, ggplot2::aes(x=pal_col, y=0, color=I(col)))+
     ggplot2::geom_point(size=20,shape=15)+
     ggplot2::facet_wrap(~palette, ncol=3,scales="free")+
     ggplot2::guides(color=FALSE)+
-    ggplot2::scale_color_manual(values = df$col)+
     ggplot2::theme_minimal()+
     ggplot2::theme(axis.ticks=ggplot2::element_blank(),
                    axis.text=ggplot2::element_blank(),
