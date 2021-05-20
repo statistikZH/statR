e87a504ab10cfbcfdc9775ceba387589304be69e
Michelle Donzallaz <michelle.donzallaz@statistik.ji.zh.ch>
2021-05-20 18:21:08 +0200

    modified example and description

--- R/interpolate.R
+++ R/interpolate.R
@@ -1,17 +1,16 @@
-# interpolate2()
 
 #'interpolate2()
 #'
-#' generate new palettes by interpolation
-#' @param palette a vector of colors to be interpolated to the same degree with the same color to generate uniform color palettes
-#' @param color a single color (hex-code) to be mixed with the colors in the 'palette'.
+#' Function to generate new color palettes by interpolation.
+#' @param palette a vector of colors to be interpolated
+#' @param color color (hex-code) to be mixed with the colors in the 'palette'.
 #' @param degree degree of interpolation, range: 1 (slight interpolation) - 7 (strong interpolation).
-#' @param number number of colours to be generated.
+#' @param number number of colors to be generated.
 #' @keywords interpolate2
 #' @export
 #' @examples
-#' #interpolate the 'zhblue'-palette with black and generate 11 new hues.
-#' interpolate2(zhpal$zhblue,"#000000",3,11)
+#' #interpolate the 'zhblue'-palette with black and generate 7 new ones
+#' interpolate2(palette = zhpal$zhblue, color = c("#000000"), degree = 3, number = 7)
 #'
 
 # Function
