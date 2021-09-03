189e4f707c6a7f7688eb680fdf10aafa3e458c9d
Michelle Donzallaz <michelle.donzallaz@statistik.ji.zh.ch>
2021-09-03 10:50:14 +0200

    removed gridlines

--- R/insert_metadata_sheet.R
+++ R/insert_metadata_sheet.R
@@ -42,41 +42,32 @@ insert_metadata_sheet <- function(wb, sheetname="Metadaten",title="Title",
 
   ## title, subtitle & Header
   style_title <- openxlsx::createStyle(fontSize=14, textDecoration="bold",fontName="Arial")
-
   style_subtitle <- openxlsx::createStyle(fontSize=12, textDecoration="bold",fontName="Arial")
-
   style_header <- openxlsx::createStyle(border="Bottom", borderColour = "#009ee0",
                                       borderStyle = getOption("openxlsx.borderStyle", "thick"))
-
   style_wrap <- openxlsx::createStyle(wrapText = TRUE)
 
 
   # Fill Excel -----------------
 
   ## Title
-  openxlsx::writeData(wb, sheet = i,title, headerStyle=style_title,startRow = 7)
+  openxlsx::writeData(wb, sheet = i, title, headerStyle=style_title, startRow = 7)
 
   ## Logo
-
   if(!is.null(logo)){
 
     if(logo == "statzh") {
-
       statzh <- paste0(.libPaths(),"/statR/extdata/Stempel_STAT-01.png")
       openxlsx::insertImage(wb, i, statzh[1], width = 2.145, height = 0.7865,
                             units = "in")
     } else if(logo == "zh"){
-
       zh <- paste0(.libPaths(),"/statR/extdata/Stempel_Kanton_ZH.png")
-
       openxlsx::insertImage(wb, i, zh[1], width = 2.145, height = 0.7865,
                             units = "in")
     } else if((logo != "statzh" | logo != "zh") & file.exists(logo)) {
-
       openxlsx::insertImage(wb, i, logo, width = 2.145, height = 0.7865,
                             units = "in")
     } else {
-
       message("no logo found and / or added")
     }
   }
@@ -112,7 +103,6 @@ insert_metadata_sheet <- function(wb, sheetname="Metadaten",title="Title",
   openxlsx::writeData(wb, sheet = i, "Hinweise:", headerStyle=style_subtitle, startRow = 12)
   openxlsx::writeData(wb, sheet = i, metadata, startRow =13)
 
-
   ## User
   if(author == "user"){
     # für das lokale R
@@ -132,14 +122,12 @@ insert_metadata_sheet <- function(wb, sheetname="Metadaten",title="Title",
                                            " durch: ", contactperson),
                       startRow = 5, startCol=12)
 
-
   # Füge Formatierungen ein
   openxlsx::addStyle(wb, sheet = i, style_header, rows = 5, cols = 1:26, gridExpand = TRUE, stack = TRUE)
-
   openxlsx::addStyle(wb, sheet = i, style_title, rows = 7, cols = 1, gridExpand = TRUE)
-
   openxlsx::addStyle(wb, sheet = i, style_subtitle, rows = 9, cols = 1, gridExpand = TRUE)
-
   openxlsx::addStyle(wb, sheet = i, style_subtitle, rows = 12, cols = 1, gridExpand = TRUE)
 
+  # remove gridlines
+  openxlsx::showGridLines(wb, sheet = i, showGridLines = FALSE)
 }
