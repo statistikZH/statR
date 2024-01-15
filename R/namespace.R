2e0368bb43d16c447791cda0cc32322f888c8a82
tknecht <thomas.knecht@statistik.ji.ktzh.ch>
2024-01-15 09:59:56 +0100

    remove from startup

--- R/namespace.R
+++ R/namespace.R
@@ -1,17 +1,8 @@
 #' @keywords internal
 .onLoad <- function(libname, pkgname) {
 
-  # TODO: do we need to give this a non-default path?
-  #       Note: this will set up the profile store on first startup,
-  #       adding the default profile from the package
-  if ((path <- Sys.getenv("STATR_CONFIG_PATH")) == "") {
-    path <- c(
-      getOption("statR_config_path"),
-      "~/.config/R/statR")[1]
-  }
 
 
-  initUserConfigStore(path)
 }
 
 #' @keywords internal
